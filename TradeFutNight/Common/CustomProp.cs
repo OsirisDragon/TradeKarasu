aca22764a918f86c4f144288f8c7a8ed8c04ba73
pomesqb <pomesqb@gmail.com>
2021-09-03 17:46:08 +0800

    little change

--- TradeFutNight/Common/CustomProp.cs
+++ TradeFutNight/Common/CustomProp.cs
@@ -5,21 +5,21 @@ namespace TradeFutNight.Common
 {
     public static class CustomProp
     {
-        public static readonly DependencyProperty NotNullNotEmpty = DependencyProperty.RegisterAttached("NotNullNotEmpty",
+        public static readonly DependencyProperty NotNullNotEmptyProperty = DependencyProperty.RegisterAttached("NotNullNotEmpty",
             typeof(bool), typeof(CustomProp), new FrameworkPropertyMetadata(null));
 
         public static bool GetNotNullNotEmpty(ContentElement element)
         {
             if (element == null)
                 throw new ArgumentNullException("element");
-            return (bool)element.GetValue(NotNullNotEmpty);
+            return (bool)element.GetValue(NotNullNotEmptyProperty);
         }
 
         public static void SetNotNullNotEmpty(ContentElement element, bool value)
         {
             if (element == null)
                 throw new ArgumentNullException("element");
-            element.SetValue(NotNullNotEmpty, value);
+            element.SetValue(NotNullNotEmptyProperty, value);
         }
     }
 }
