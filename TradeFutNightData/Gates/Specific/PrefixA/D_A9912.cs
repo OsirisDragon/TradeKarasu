bd33743cb9e92ec81de67364e6b2919d947df54d
pomesqb <pomesqb@gmail.com>
2021-11-22 10:26:08 +0800

    adjust sql

--- TradeFutNightData/Gates/Specific/PrefixA/D_A9912.cs
+++ TradeFutNightData/Gates/Specific/PrefixA/D_A9912.cs
@@ -15,15 +15,14 @@ namespace TradeFutNightData.Gates.Specific.PrefixA
         public IEnumerable<T> ListByDate(DateTime START_DATE, DateTime END_DATE, string PROD_ID)
         {
             var sql = @"
-SELECT 	A.*,
-			'' AS op_type,
-			B.PDK_SUBTYPE
-FROM PHALT A
-LEFT JOIN PDK B ON A.PHALT_PROD_ID = B.PDK_KIND_ID
-WHERE
-( (@START_DATE  IS NULL OR PHALT_TRADE_DATE >= @START_DATE) AND (@END_DATE IS NULL OR PHALT_TRADE_DATE <= @END_DATE) )
-AND ( @PROD_ID = '' OR PHALT_PROD_ID = @PROD_ID )
-ORDER BY PHALT_TRADE_DATE DESC, PHALT_PROD_ID ASC, PHALT_TRADE_PAUSE_DATE DESC
+                        SELECT 	A.*,
+		                        B.PDK_SUBTYPE
+                        FROM PHALT A
+                        LEFT JOIN PDK B ON A.PHALT_PROD_ID = B.PDK_KIND_ID
+                        WHERE
+                        ( (@START_DATE  IS NULL OR PHALT_TRADE_DATE >= @START_DATE) AND (@END_DATE IS NULL OR PHALT_TRADE_DATE <= @END_DATE) )
+                        AND ( @PROD_ID = '' OR PHALT_PROD_ID = @PROD_ID )
+                        ORDER BY PHALT_TRADE_DATE DESC, PHALT_PROD_ID ASC, PHALT_TRADE_PAUSE_DATE DESC
                         ";
 
             var p = new DynamicParameters();
