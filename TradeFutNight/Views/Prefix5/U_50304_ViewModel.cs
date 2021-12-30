1433b58f629d84652f035544fc335607f9bcf537
sandylo <samdy800322@gmail.com>
2021-12-30 09:37:20 +0800

    新增

---
+++ TradeFutNight/Views/Prefix5/U_50304_ViewModel.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using TradeFutNightData;
+using TradeFutNightData.Gates.Specific.Prefix5;
+using TradeFutNightData.Models.Common;
+
+namespace TradeFutNight.Views.Prefix5
+{
+    public class U_50304_ViewModel : ViewModelParent<UIModel_50304>
+    {
+        public U_50304_ViewModel()
+        {
+            MainGridData = new ObservableCollection<UIModel_50304>();
+        }
+
+        public async void Open()
+        {
+            await Query();
+        }
+
+        public void Insert()
+        {
+            MainGridData.Insert(0, new UIModel_50304());
+        }
+
+        public void Delete(object item)
+        {
+            MainGridData.Remove((UIModel_50304)item);
+        }
+
+        public async Task Query()
+        {
+            MapperInstance = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<MWPLMT, UIModel_50304>().ReverseMap();
+            }));
+
+            var task = Task.Run(() =>
+            {
+                using (var das = Factory.CreateDalSession())
+                {
+                    var d50304 = new D_50304<UIModel_50304>(das);
+                    MainGridData = d50304.List().ToList();
+                }
+            });
+
+            await task;
+        }
+    }
+
+    public class UIModel_50304 : MPR
+    {
+        public virtual string PROD_ID_OUT { get; set; }
+        public virtual string PROD_SETTLE_DATE { get; set; }
+    }
+}
---
+++ TradeFutNightData/Gates/Specific/Prefix5/D_50304.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using DataEngine;
+using System.Collections.Generic;
+
+namespace TradeFutNightData.Gates.Specific.Prefix5
+{
+    public class D_50304<T> : ParentGate
+    {
+        public D_50304(DalSession das)
+        {
+            this._das = das;
+        }
+
+        public IEnumerable<T> List()
+        {
+            var sql = @"
+                        SELECT  MPR_ORIG_TIME  ,
+                                PROD_ID_OUT ,
+                                MPR_M_PRICE ,
+                                MPR_M_QNTY ,
+                                PROD_SETTLE_DATE
+                        FROM MPR, PROD
+                        WHERE MPR_PROD_ID = PROD_ID
+                        AND MPR_M_QNTY <> 0
+                        AT ISOLATION 0
+                        ";
+
+            var result = _das.Conn.Query<T>(BuildCommand<T>(sql, null));
+
+            return result;
+        }
+    }
+}
