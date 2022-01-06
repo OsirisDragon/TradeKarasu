296aaeafcadbc49da515fff1e89d0fec2c1db6d3
sandylo <samdy800322@gmail.com>
2022-01-06 09:32:18 +0800

    新增排序

--- TradeFutNightData/Gates/Specific/Prefix5/D_50304.cs
+++ TradeFutNightData/Gates/Specific/Prefix5/D_50304.cs
@@ -14,14 +14,15 @@ namespace TradeFutNightData.Gates.Specific.Prefix5
         public IEnumerable<T> List()
         {
             var sql = @"
-                        SELECT  MPR_ORIG_TIME  ,
-                                PROD_ID_OUT ,
-                                MPR_M_PRICE ,
-                                MPR_M_QNTY ,
+                        SELECT  MPR_ORIG_TIME,
+                                PROD_ID_OUT,
+                                MPR_M_PRICE,
+                                MPR_M_QNTY,
                                 PROD_SETTLE_DATE
                         FROM MPR, PROD
                         WHERE MPR_PROD_ID = PROD_ID
                         AND MPR_M_QNTY <> 0
+                        ORDER BY PROD_ID_OUT, PROD_SETTLE_DATE, MPR_ORIG_TIME
                         AT ISOLATION 0
                         ";
 
