588d77f2a8da896a7a4132a3a60147960c31e8f3
sandylo <samdy800322@gmail.com>
2022-02-10 11:16:32 +0800

    新增

---
+++ TradeFutNight/Views/Prefix5/U_50307.xaml.cs
@@ -0,0 +1,155 @@
+﻿using CrossModel;
+using CrossModel.Enum;
+using DevExpress.XtraReports.UI;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using TradeFutNight.Common;
+using TradeFutNight.Interfaces;
+using TradeFutNight.Reports;
+
+namespace TradeFutNight.Views.Prefix5
+{
+    /// <summary>
+    /// U_50307.xaml 的互動邏輯
+    /// </summary>
+    public partial class U_50307 : UserControlParent, IViewSword
+    {
+        private U_50307_ViewModel _vm;
+
+        public U_50307()
+        {
+            InitializeComponent();
+            _vm = (U_50307_ViewModel)DataContext;
+        }
+
+        public async Task<bool> IsCanRun()
+        {
+            var task = Task.Run(() =>
+            {
+                var isCanRun = IsCanRunProgram();
+                DbLog(MessageConst.IsCanRun + ":" + isCanRun.ToString().ToUpper());
+                return isCanRun;
+            });
+            await task;
+
+            return task.Result;
+        }
+
+        public override void ToolButtonSetting()
+        {
+            base.ToolButtonSetting();
+            VmMainUi.IsButtonPrintEnabled = true;
+        }
+
+        public async Task Open()
+        {
+            ToolButtonSetting();
+
+            var task = Task.Run(() =>
+            {
+                _vm.Open();
+                DbLog(MessageConst.Open);
+            });
+            await task;
+        }
+
+        public void Insert()
+        {
+            gridView.CloseEditor();
+            _vm.Insert();
+        }
+
+        public void Delete()
+        {
+            bool isNeedConfirm = true;
+            var selectedItem = gridMain.SelectedItem;
+            if (selectedItem != null)
+            {
+                if (isNeedConfirm)
+                {
+                    if (MessageBoxExService.Instance().Confirm(MessageConst.ConfirmDelete) == MessageBoxResult.Yes)
+                        _vm.Delete(selectedItem);
+                }
+                else
+                {
+                    _vm.Delete(selectedItem);
+                }
+            }
+        }
+
+        public async Task<bool> CheckField()
+        {
+            if (!BaseCheck(new CheckSettings() { IsCheckNotNullNotEmpty = false }, gridMain, _vm))
+                return false;
+
+            var task = Task.Run(() =>
+            {
+                if (!IsCanRunProgram())
+                {
+                    VmMainUi.HideLoadingWindow();
+                    MessageBoxExService.Instance().Error(MessageConst.NotAllowedExcute);
+                    return false;
+                }
+
+                return true;
+            });
+            await task;
+
+            return task.Result;
+        }
+
+        public async Task Save()
+        {
+            var task = Task.Run(() =>
+           {
+           });
+            await task;
+        }
+
+        private XtraReport CreateReport<T>(IList<T> data, OperationType operationType)
+        {
+            string reportTitle = ProgramID + "–" + ProgramName;
+
+            var rptSetting = ReportNormal.CreateSetting(ProgramID, reportTitle, UserName, Memo, Ocf.OCF_DATE, false, false, false);
+            var reportCommon = ReportNormal.CreateCommonPortrait(data, gridMain, rptSetting);
+
+            return reportCommon;
+        }
+
+        public async Task Export()
+        {
+            gridView.CloseEditor();
+            await Task.FromResult<object>(null);
+            throw new NotImplementedException();
+        }
+
+        public async Task Print()
+        {
+            gridView.CloseEditor();
+
+            var report = CreateReport(_vm.MainGridData, OperationType.Print);
+
+            var reportGate = await new ReportGate(report).CreateDocumentAsync();
+            await reportGate.ExportPdf(GetExportFilePath());
+            await reportGate.Print();
+
+            MessageBoxExService.Instance().Info(MessageConst.PrintSuccess);
+        }
+
+        public async Task PrintIndex()
+        {
+            gridView.CloseEditor();
+            await Task.FromResult<object>(null);
+            throw new NotImplementedException();
+        }
+
+        public async Task PrintStock()
+        {
+            gridView.CloseEditor();
+            await Task.FromResult<object>(null);
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TradeFutNight/Views/Prefix5/U_50307_ViewModel.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using ChangeTracking;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using TradeFutNightData;
+using TradeFutNightData.Gates.Specific.Prefix5;
+using TradeFutNightData.Models.Common;
+
+namespace TradeFutNight.Views.Prefix5
+{
+    public class U_50307_ViewModel : ViewModelParent<UIModel_50307>
+    {
+        public U_50307_ViewModel()
+        {
+            MainGridData = new ObservableCollection<UIModel_50307>();
+        }
+
+        public async void Open()
+        {
+            await Query();
+        }
+
+        public void Insert()
+        {
+            MainGridData.Insert(0, new UIModel_50307());
+        }
+
+        public void Delete(object item)
+        {
+            MainGridData.Remove((UIModel_50307)item);
+        }
+
+        public async Task Query()
+        {
+            MapperInstance = new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<PROD, UIModel_50307>().ReverseMap();
+            }));
+
+            var task = Task.Run(() =>
+            {
+                IList<UIModel_50307> list = null;
+
+                using (var das = Factory.CreateDalSession())
+                {
+                    var d50307 = new D_50307<UIModel_50307>(das);
+                    list = d50307.List().ToList();
+                }
+
+                MainGridData = list.AsTrackable();
+            });
+
+            await task;
+        }
+    }
+
+    public class UIModel_50307 : PROD
+    {
+    }
+}
