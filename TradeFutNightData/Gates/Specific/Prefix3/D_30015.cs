8fd1ea66ae66e18ece478110e43f478b9cd16be0
sandylo <samdy800322@gmail.com>
2022-02-15 13:54:42 +0800

    新增

---
+++ TradeFutNightData/Gates/Specific/Prefix3/D_30015.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using DataEngine;
+using System.Collections.Generic;
+
+namespace TradeFutNightData.Gates.Specific.Prefix3
+{
+    public class D_30015<T> : ParentGate
+    {
+        public D_30015(DalSession das)
+        {
+            this._das = das;
+        }
+
+        public IEnumerable<T> List()
+        {
+            var sql = @"
+                        SELECT  BLOT_KIND_ID,
+                                BLOT_MAX,
+                                BLOT_MIN,
+                                BLOT_MIN_QNTY
+                        FROM BLOT
+                        JOIN
+                        (
+                            SELECT DISTINCT PDK_PARAM_KEY
+                            FROM PDK WHERE PDK_STATUS_CODE <> '0'
+                        ) B
+                        ON BLOT_KIND_ID = B.PDK_PARAM_KEY
+                        ";
+
+            var result = _das.Conn.Query<T>(BuildCommand<T>(sql, null));
+
+            return result;
+        }
+    }
+}
