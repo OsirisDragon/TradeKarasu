4a7a8fae8d7ef125eaaa531a8dc0cd1233263833
sandylo <samdy800322@gmail.com>
2022-06-10 10:43:43 +0800

    新增作業

---
+++ TradeFutNightData/Models/Common/PDK2ND.cs
@@ -0,0 +1,20 @@
+﻿using CrossModel;
+using LinqToDB.Mapping;
+using System;
+
+namespace TradeFutNightData.Models.Common
+{
+    public class PDK2ND : DtoParent<PDK2ND>
+    {
+        [PrimaryKey]
+        public virtual string PDK2ND_KIND_ID { get; set; } // char(4)
+
+        public virtual decimal PDK2ND_UNIT { get; set; } // decimal(11, 7)
+        public virtual decimal PDK2ND_UNIT_SPREAD { get; set; } // decimal(11, 7)
+        public virtual char PDK2ND_PRICE_FLUC { get; set; } // char(1)
+        public virtual string PDK2ND_USER_ID { get; set; } // char(5)
+        public virtual DateTime PDK2ND_W_TIME { get; set; } // datetime
+        public virtual char PDK2ND_PRICE_TYPE { get; set; } // char(1)
+        public virtual char PDK2ND_TPP_PRICE_TYPE { get; set; } // char(1)
+    }
+}
