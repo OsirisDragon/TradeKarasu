bfb7fa57fe1557fe0f26480227c1f02e8bad1546
sandylo <samdy800322@gmail.com>
2022-06-29 09:31:47 +0800

    新增80014

--- TradeFutNightData/Gates/Common/D_UTP.cs
+++ TradeFutNightData/Gates/Common/D_UTP.cs
@@ -2,6 +2,7 @@
 using DataEngine;
 using LinqToDB;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using TradeFutNightData.Models.Common;
 
@@ -48,6 +49,28 @@ namespace TradeFutNightData.Gates.Common
             var result = _das.Conn.Execute(BuildCommand<T>(sql, p));
 
             return result;
+        }
+
+        public IEnumerable<UTP> ListByTxnAndUser(string TXN_ID, string USER_ID)
+        {
+            var query = _das.DataConn.GetTable<UTP>()
+                .Where(c => c.UTP_TXN_ID == TXN_ID && c.UTP_USER_ID == USER_ID);
+            return query.ToList();
+        }
+
+        public void InsertItem(UTP item)
+        {
+            _das.DataConn.Insert(item);
+        }
+
+        public void UpdateItem(UTP item)
+        {
+            _das.DataConn.GetTable<UTP>()
+                .Where(c => c.UTP_USER_ID == item.UTP_USER_ID && c.UTP_TXN_ID == item.UTP_TXN_ID)
+                .Set(c => c.UTP_YN_CODE, item.UTP_YN_CODE)
+                .Set(c => c.UTP_W_DATE, item.UTP_W_DATE)
+                .Set(c => c.UTP_W_USER_ID, item.UTP_W_USER_ID)
+                .Update();
         }
     }
 }
